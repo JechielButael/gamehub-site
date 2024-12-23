8343e4f29d26d7b38d2bebf5552c618ee147befe
JechielButael <146856404+JechielButael@users.noreply.github.com>
2024-12-24 01:50:35 +0200

    removing api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "b82cd03832284058af31c1a2f2d2befe",
+    key: process.env.REACT_APP_API_KEY,
   },
 });
